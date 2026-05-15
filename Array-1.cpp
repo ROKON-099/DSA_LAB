c116ec04229ff404d7f6597ac9bb82dc802b59dd
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-05-15 08:39:23 +0600

    2nd commit

--- Array-1.cpp
+++ Array-1.cpp
@@ -1,1 +1,46 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout << "Enter the size of array:";
+    cin>>n;
+    cout << "Enter " <<n << " Number of array:";
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int key ;
+    cout << "Enter the searching item:";
+    cin>>key;
+    bool found =false;
+    int index=-1;
+
+
+    for (int i=0; i<n; i++)
+    {
+        if (arr[i]==key ) {
+            found= true ;
+            index=i;
+            break;
+        }
+
+
+    }
+    if (found)
+        cout << "element found at index:" <<index;
+        else
+            cout << "element is not found";
+        return 0;
+}
+
+
+
+
+
+
+
+
+
 
